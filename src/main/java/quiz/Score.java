b50cd483ab1fc07f5aa4b225cab21d61e25fe63a
Radu Ghita <radu.ghita811@yahoo.com>
2022-06-02 17:49:27 +0300

    Twelfth commit - Added missing files.

---
+++ src/main/java/quiz/Score.java
@@ -0,0 +1,19 @@
+package quiz;
+
+public class Score {
+    String nickname;
+    int points;
+
+    public Score(String nickname, int points) {
+        this.nickname = nickname;
+        this.points = points;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+}
